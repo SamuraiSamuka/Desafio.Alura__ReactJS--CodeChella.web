2fcafdbf653d2ea87bc8b5f6c72f23ca552fb4e0
Samuel Silva <samuel.carvalho.dev@gmail.com>
2023-02-27 22:46:25 -0300

    corrigindo deploy

--- src/Components/Botao/index.jsx
+++ src/Components/Botao/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { MdLocalActivity } from "react-icons/Md"
+import { HiTicket } from "react-icons/Hi"
 import "./Botao.css"
 
 export default function Botao({children}) {
   return (
     <button type='button' className='botao'>
         {children}
-        <MdLocalActivity />
+        <HiTicket />
     </button>
   )
 }

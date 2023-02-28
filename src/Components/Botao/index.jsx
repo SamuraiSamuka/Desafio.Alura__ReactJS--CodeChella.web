6618db5a089ad1a0f5ef3e1177e0345964f3055c
Samuel Silva <samuel.carvalho.dev@gmail.com>
2023-02-27 22:47:22 -0300

    corrigindo deploy

--- src/Components/Botao/index.jsx
+++ src/Components/Botao/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import { HiTicket } from "react-icons/Hi"
 import "./Botao.css"
 
 export default function Botao({children}) {
   return (
     <button type='button' className='botao'>
         {children}
-        <HiTicket />
     </button>
   )
 }
